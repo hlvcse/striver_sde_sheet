bbe89761e2036252e048f87eb340319feeb7a7d0
Hanumat Lal Vishwakarma <106891939+hlvcse@users.noreply.github.com>
2022-07-12 23:14:45 -0800

    Update 12.CountInversions.cpp

--- 12.CountInversions.cpp
+++ 12.CountInversions.cpp
@@ -1,57 +1,45 @@
-#include <bits/stdc++.h> 
-long long merge(long long *arr,int left,int mid,int right)
-{
-
-    long long inv=0;
-    long long temp[right-left+1],ind=0;
-    int i=left,j=mid+1;
-    while(i<=mid && j<=right)
+long long merge(long long nums[],int low,int mid,int high)
     {
-       if(arr[i]<=arr[j])
-       {
-           temp[ind++]=arr[i++];
-       }
-       else
-       {
-           temp[ind++]=arr[j++];
-           inv+=(mid-i+1);
-       }     
-      }
-    while(i<=mid)temp[ind++]=arr[i++];
-    while(j<=right)temp[ind++]=arr[j++];
-    ind=0;
-    for(int i=left;i<=right;i++)
-        arr[i]=temp[ind++];
-    
-    return inv;
-    
-}
-long long merge_sort(long long *arr,int left,int right)
-{
-    if(left==right) return 0;
-    if(left<right){
-       long long left_in=merge_sort(arr,left,(left+right)/2);
-        long long right_in=merge_sort(arr,(left+right)/2+1,right);
-        return left_in+right_in+merge(arr,left,(left+right)/2,right);
+        long long inv=0,j=mid+1,i=low;
+        //count inversions.
+        for(;i<=mid;i++){
+            while(j<=high && nums[i] >  nums[j]){
+                j++;
+            }
+            inv+=j-(mid+1);
+        }
+            
+        //merging..
+        i=low,j=mid+1;
+        int t[high-low+1],ind=0;//temporary array
+        while(i<=mid && j<= high){
+            if(nums[i]<=nums[j]){
+                t[ind++]=nums[i++];
+            }
+            else{
+                t[ind++]=nums[j++];
+            }
+        }
+        while(i<=mid){
+                t[ind++]=nums[i++];
+            }
+        while(j<=high){
+                t[ind++]=nums[j++];
+            }
+        ind=0;
+        for(int i=low;i<=high;i++)
+            nums[i]=t[ind++];
+        return inv;
+
     }
-    
+long long mergeSort(long long nums[],int low,int high)
+ {
+        if(low==high)return 0;
+        int mid=(low+high)/2;
+        long long left=mergeSort(nums,low,mid);
+        long long right=mergeSort(nums,mid+1,high);
+        return left + right + merge(nums,low,mid,high);
 }
 long long getInversions(long long *arr, int n){
-    // Write your code here.
-    //brute-force..TLE.
-    /*
-    long long count=0;
-    for(int i=0;i<n-1;i++)
-    {
-        for(int j=i+1;j<n;j++)
-       {
-         if(arr[j]<arr[i])
-             count++;
-       }
-    }
-    return count;*/
-    //method.2 //merge sort O(nlog n)
-    long long count=merge_sort(arr,0,n-1);
-  
-    return count;
+    return mergeSort(arr,0,n-1);
 }
